227440a617a3d855acccc2aafe03574dadb5858e
storycoding <youknownuno@gmail.com>
2018-08-17 16:12:33 +0100

    init: initiated basic repo

---
+++ nuxt-vuetify/nuxt.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-vuetify',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: ['~/assets/main.css'],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /* to remove warning on build
+    { 
+      statusCode: 404,
+      path: '/vuetify.css.map',
+      message: 'This page could not be found'
+    }
+    */
+    cssSourceMap: false, 
+
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  modules: [
+    '@nuxtjs/vuetify'
+  ],
+
+  vuetify: {
+    // Vuetify options
+    theme: {
+      primary: '#1976D2',
+      secondary: '#424242',
+      accent: '#82B1FF',
+      error: '#FF5252',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FFC107'
+    }
+  }
+}
+
---
+++ nuxt-vuetify/pages/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-app dark>
+    <section class="container">
+      <div>
+        <v-btn color="info">Info</v-btn>
+        <v-btn color="primary">Primary</v-btn>
+        <app-logo/>
+
+        <v-container>
+          <v-layout row wrap>
+            <v-flex xs1>
+              <v-card dark color="primary">
+                <v-card-text class="px-0">v-card</v-card-text>
+              </v-card>
+              <v-card dark color="primary">
+                <v-card-text class="px-0">v-card</v-card-text>
+              </v-card>
+              <v-card dark color="primary">
+                <v-card-text class="px-0">v-card</v-card-text>
+              </v-card>
+              <v-card dark color="primary">
+                <v-card-text class="px-0">v-card</v-card-text>
+              </v-card>
+              <v-card dark color="primary">
+                <v-card-text class="px-0">v-card</v-card-text>
+              </v-card>
+              <div class="square">orange</div>
+              <div class="square">orange</div>
+              <div class="square">orange</div>
+            </v-flex>
+          </v-layout>
+
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="error">
+          <v-card-text class="px-0 m-2">12</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0 m-2">6</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 3" :key="`4${i}`" xs4>
+        <v-card dark color="error">
+          <v-card-text class="px-0 m-2">4</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0 m-2">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 6" :key="`2${i}`" xs2 m-2 p-2>
+        <v-card dark color="error">
+          <v-card-text class="px-0 m-2">2</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0 m-2">1</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+
+        </v-container>
+
+        <h1 class="title">
+          nuxt-vuetify
+        </h1>
+        <h2 class="subtitle">
+          Nuxt.js project
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </div>
+      </div>
+    </section>
+  </v-app>
+</template>
+<v-app>
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+
+* {
+  width: 200px:
+  height: 200px;
+  color: #FF5252;
+  background-color: orange;
+  border-radius: 10px;
+}
+</style>
+
